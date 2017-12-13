2beb8351f55983b41f62f22ab05a763638ac2c56
Thomas Daniel <thomas@ampervue.com>
2017-12-13 23:47:37 +0000

    added docker push

--- az-container-create.sh
+++ az-container-create.sh
@@ -3,15 +3,14 @@
 az acr login --name AmpervueContainers
 az acr update --name AmpervueContainers --admin-enabled true
 
+docker push ampervuecontainers.azurecr.io/ampervue-polls
+
 # put all env variables on a single line
 vars="$(sed -e :a -e '$!N; s/\n/ /; ta' secrets.env)"
-echo $vars
 
 # get the registry password
 password=$(az acr credential show -n AmpervueContainers -o table | awk '$1 == "AmpervueContainers" { print $2; }')
-echo "password=$password"
 login_server=$(az acr list -o table | awk '$1 == "AmpervueContainers" { print $5; }')
-echo "login_server=$login_server"
 
 az container create --name polls \
     --image ampervuecontainers.azurecr.io/ampervue-polls \
