a0fe20af321ab819c1107713c565b829ac63168a
Thomas Daniel <thomas@ampervue.com>
2018-01-08 20:49:52 +0000

    fixed errors with password in quotes

--- az-container-create.sh
+++ az-container-create.sh
@@ -3,15 +3,15 @@
 image_name=${1:?Usage: $0 IMAGE [CONTAINER]}
 container_name=${2:-$image_name}
 
-# This is required once, but it is fast, so we do it every time
-az acr login --name $registry
-az acr update --name $registry --admin-enabled true
+# This is required once
+# az acr login --name $registry
+# az acr update --name $registry --admin-enabled true
 
 # Read environment variables
 vars=( $(cat secrets.env) )
 
 # get the registry password
-password=$(az acr credential show -n AmpervueContainers --query 'passwords[0].value')
+password=$(az acr credential show -n AmpervueContainers --query 'passwords[0].value' | sed -e 's/"//g')
 resource_group=$(az acr list --query '[0].resourceGroup' | sed -e 's/"//g')
 registry=$(az acr list --query '[0].name' | sed -e 's/"//g' | tr A-Z a-z)
 login_server=$(az acr list --query '[0].loginServer' | sed -e 's/"//g')
